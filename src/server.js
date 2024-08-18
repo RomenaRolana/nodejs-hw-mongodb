ec266ac41b596795c5084c6b35c4be88655a8cc8
Bozhenko Maksym <93256327+RomenaRolana@users.noreply.github.com>
2024-08-18 12:19:45 +0300

    hw3vol3

--- src/server.js
+++ src/server.js
@@ -1,12 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
-
 import dotenv from 'dotenv';
 import { env } from './utils/env.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
-
 import contactsRouter from './routers/contacts.js';
 
 dotenv.config();
@@ -16,28 +14,26 @@ const PORT = Number(env('PORT', '3000'));
 export const setupServer = () => {
   const app = express();
 
-  app.use(
-    express.json({
-      type: ['application/json', 'application/vnd.api+json'],
-      limit: '100kb',
-    }),
-  );
+  app.use(express.json({
+    type: ['application/json', 'application/vnd.api+json'],
+    limit: '100kb',
+  }));
+
   app.use(cors());
 
-  app.use(
-    pino({
-      transport: {
-        target: 'pino-pretty',
-      },
-    }),
-  );
+  app.use(pino({
+    transport: {
+      target: 'pino-pretty',
+    },
+  }));
 
-  // Додаємо роутер
+  // Додаємо роутер для контактів
   app.use(contactsRouter);
 
+  // Обробник для неіснуючих маршрутів
+  app.use(notFoundHandler);
 
-  app.use('*', notFoundHandler);
-
+  // Глобальний обробник помилок
   app.use(errorHandler);
 
   // Запуск сервера
